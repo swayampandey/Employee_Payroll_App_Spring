3719f4a5820da49e89897fb0bbca98087f47bbbb
Swayam Prakash <swayam265@gmail.com>
2022-10-17 00:32:23 +0530

    [swayam]: added RestController in employee payroll app

---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.employeepayrollapp.dto;
+
+public class EmployeePayrollDTO {
+    public String name;
+    public long salary;
+
+    public EmployeePayrollDTO(String name, long salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
