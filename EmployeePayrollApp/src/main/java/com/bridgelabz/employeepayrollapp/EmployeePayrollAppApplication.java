f9fb211f28bef6885ccb79a26a9c0cd297fb7641
Swayam Prakash <swayam265@gmail.com>
2022-10-18 12:09:57 +0530

    [Swayam] : added functionality to use lombok library for logging of employee payroll app

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
+
 public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
