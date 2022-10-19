8f4c7330b92d1b365c0a506a6285091e55e62ce9
Swayam Prakash <swayam265@gmail.com>
2022-10-19 10:26:11 +0530

    [Swayam] : added functionality of name validation for rest call validation in employee payroll app

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,9 +1,20 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
 import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+
 @Data
 public class EmployeePayrollDTO {
+    @NotEmpty(message = "Employee name cannot be empty")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     public String name;
+    @Min(value = 500, message = "Salary should be more than 500")
+    @Max(value = 900000, message = "Salary should not exceeds 900000")
     public long salary;
 
 }
--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -53,4 +53,3 @@ public class EmployeePayrollData {
         this.salary = salary;
     }
 }
-
