673ad699648355862342111d39d0ef9c553792d3
Swayam Prakash <swayam265@gmail.com>
2022-10-19 11:57:23 +0530

    [Swayam] : added functionality of user friendly error responsein case validation fails in employee payroll app

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 
 
 public class EmployeePayrollData {
@@ -15,41 +16,10 @@ public class EmployeePayrollData {
     private long salary;
 
 
-
     public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
         this.employeeId = employeeId;
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
     }
 
-    public int getEmployeeId() {
-
-        return employeeId;
-    }
-
-
-    public void setEmployeeId(int employeeId) {
-
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-
-        this.name = name;
-    }
-
-    public long getSalary() {
-
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-
-        this.salary = salary;
-    }
 }
